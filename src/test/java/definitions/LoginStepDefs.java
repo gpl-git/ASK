abaac50f21d26837c2efc3b903563ab9c0f8c612
KrasiBl <Krasi7811093455!>
2020-06-10 15:38:48 -0500

    Updated

--- src/test/java/definitions/LoginStepDefs.java
+++ src/test/java/definitions/LoginStepDefs.java
@@ -14,16 +14,16 @@ public class LoginStepDefs {
     public void iOpenPage(String page) {
         if (page.equals("login")) {
             getDriver().get("http://ask-qa.portnov.com/#/login");
-        }else if (page.equals("registration")){
+        } else if (page.equals("registration")) {
             getDriver().get("http://ask-qa.portnov.com/#/registration");
-        }else {
+        } else {
             System.out.println("Unsupported site");
         }
     }
 
     @And("I wait for {int} sec")
     public void iWaitForSec(int sec) throws InterruptedException {
-        Thread.sleep(sec*1000);
+        Thread.sleep(sec * 1000);
     }
 
     @When("I type {string} into email field")
@@ -43,9 +43,73 @@ public class LoginStepDefs {
 
     @Then("text {string} is displayed")
     public void textIsDisplayed(String text) {
-        String role = getDriver().findElement(By.xpath("//p[contains(text(),'"+text+"')]")).getText();
+        String role = getDriver().findElement(By.xpath("//p[contains(text(),'" + text + "')]")).getText();
         assertThat(role.equals(text)).isTrue();
     }
 
 
+    @And("I navigate to the Settings page")
+    public void iNavigateToTheSettingsPage() { getDriver().findElement(By.xpath("//*[contains(text(),'Settings')]")).click();
+    }
+
+
+
+    @And("I click Change Your Name button")
+    public void iClickChangeYourNameButton() {getDriver().findElement(By.xpath("//*[contains(text(),'Change Your Name')]")).click();
+    }
+
+    @Then("{string} is displayed")
+    public void isDisplayed(String arg0) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'Change Your Name')]"));
+    }
+
+    @Then("I clear the field")
+    public void iClearTheField() {
+        getDriver().findElement(By.xpath("//*[@placeholder='Full Name']")).clear();
+
+    }
+
+    @Then("I change Student name to {string}")
+    public void iChangeStudentNameTo(String name) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='name']")).sendKeys(name);
+        getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//span[@class='mat-button-wrapper'][contains(text(),'Change')]")).click();
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + name + "')]")).getText();
+        assertThat(name.equals(name)).isTrue();
+        System.out.println("You changed your student username to " + name + "!");
+    }
+
+    @And("I log out")
+    public void iLogOut() {
+        getDriver().findElement(By.xpath("//*[contains(text(),'Log Out')]")).click();
+    }
+
+
+    @And("I navigate to the User management page")
+    public void iNavigateToTheUserManagementPage() {
+        getDriver().findElement(By.xpath("//*[contains(text(),'Users Management')]")).click();
+    }
+
+    @Then("I click to {string} from mat-card menu")
+    public void iClickToFromMatCardMenu(String students) {
+        getDriver().findElement(By.xpath("//div[contains(text(),'Students')]")).click();
+
+    }
+
+
+
+    @And("I click to student {string}")
+    public void iClickToStudent(String number) {
+        getDriver().findElement(By.xpath("//a["+3+"]//mat-list-item[1]")).click();
+    }
+
+    @And("I click on options button")
+    public void iClickOnOptionsButton() {
+        getDriver().findElement(By.xpath("//*[@class='mat-button-wrapper']")).click();
+
+
+    }
 }
+
+
+//    getDriver().findElement(By.xpath("//div[@class='cdk-overlay-container']//button[1]")).click();
+//        getDriver().findElement(By.xpath("//*[@placeholder='Full Name']]")).clear();
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,6 +1,8 @@
 Feature: Login Scenarios
+
   Background:
     Given I open "login" page
+
   Scenario Outline: Login - Happy Path
     When I type "<email>" into email field
     And I type "<password>" into password field
@@ -8,9 +10,68 @@ Feature: Login Scenarios
     And I wait for 1 sec
     Then text "<text>" is displayed
     Examples:
-      |  email            | password | text   |
-      | ask_instr@aol.com | 12345   |TEACHER  |
-      |  test8@test.com   | 12345   | STUDENT |
+      | email             | password | text    |
+      | ask_instr@aol.com | 12345    | TEACHER |
+      | test8@test.com    | 12345    | STUDENT |
+
+#    Automate test set User management - change Student name
+#    Change options change Student name
+  Scenario Outline: Change Student name
+    When I type "<email>" into email field
+    And I type "<password>" into password field
+    When I click SignIn button
+    And I wait for 1 sec
+    Then text "<text>" is displayed
+    And I navigate to the Settings page
+    And I click Change Your Name button
+    And I wait for 1 sec
+    Then "Change Your Name" is displayed
+    Then I clear the field
+    Then I change Student name to "<name>"
+    And I wait for 1 sec
+    And I log out
+    Examples:
+      | email          | password | text    | name          |
+      | test8@test.com | 12345    | STUDENT | OLEG IVANOV   |
+      | test9@test.com | 12345    | STUDENT | IVAN IVANOV   |
+
+
+    #    Automate test set "User management - change Student name
+    #    Change options change Student name
+  Scenario Outline: Teacher change Student name
+    When I type "<email>" into email field
+    And I type "<password>" into password field
+    When I click SignIn button
+    And I wait for 1 sec
+    Then text "<text>" is displayed
+    And I navigate to the User management page
+    Then I click to "Students" from mat-card menu
+    And I click to student "row number"
+    And I click on options button
+#
+    And I click Change Your Name button
+    And I wait for 1 sec
+    Then "Change Name" is displayed
+    Then I clear the field
+    Then I change Student name to "<name>"
+    And I wait for 1 sec
+    And I log out
+    Examples:
+      | email            | password | text    | name        |
+      |ask_instr@aol.com | 12345    | TEACHER | IVAN IVANOV |
+
+
+
+#    Change options change Student group
+  Scenario Outline: Change Student group
+    When I type "<email>" into email field
+    And I type "<password>" into password field
+    When I click SignIn button
+    And I wait for 1 sec
+    Then text "<text>" is displayed
+    Examples:
+      | email | password | text |
+      | test8@test.com    | 12345    | STUDENT |
 
 
 
